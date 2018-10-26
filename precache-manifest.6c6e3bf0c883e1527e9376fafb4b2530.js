903c552b9f575422da59dec7d214a9ae071d07bf
developer <raz.dechantal@gmail.com>
2018-10-26 12:38:21 +0200

    Updates

---
+++ precache-manifest.6c6e3bf0c883e1527e9376fafb4b2530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c9c085a1776314c1812c",
+    "url": "/hello/static/js/runtime~main.c9c085a1.js"
+  },
+  {
+    "revision": "fd784e2300a6844986ce",
+    "url": "/hello/static/js/main.fd784e23.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/hello/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "fd784e2300a6844986ce",
+    "url": "/hello/static/css/main.aeb4430a.chunk.css"
+  },
+  {
+    "revision": "4f2aaaf58bd829869916dbe28971c526",
+    "url": "/hello/index.html"
+  }
+];
