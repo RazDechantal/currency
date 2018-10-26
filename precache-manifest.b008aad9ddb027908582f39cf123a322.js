484b076f273923e2694ea3cc840959ca184b0e32
developer <raz.dechantal@gmail.com>
2018-10-26 13:27:12 +0200

    Updates

---
+++ precache-manifest.b008aad9ddb027908582f39cf123a322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5825a23d130a1ad4a778",
+    "url": "/currency/static/js/runtime~main.5825a23d.js"
+  },
+  {
+    "revision": "e0905391b5c1958046f5",
+    "url": "/currency/static/js/main.e0905391.chunk.js"
+  },
+  {
+    "revision": "669015efdb9f77fc861a",
+    "url": "/currency/static/js/1.669015ef.chunk.js"
+  },
+  {
+    "revision": "e0905391b5c1958046f5",
+    "url": "/currency/static/css/main.6c8dc2e8.chunk.css"
+  },
+  {
+    "revision": "669015efdb9f77fc861a",
+    "url": "/currency/static/css/1.2c17f9d7.chunk.css"
+  },
+  {
+    "revision": "37e7a7f37ad6d45e9de1ef37609bb32a",
+    "url": "/currency/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hello/precache-manifest.4840b38e3010cad6a9a9fcb5d4d7cd2c.js"
+  "/currency/precache-manifest.b008aad9ddb027908582f39cf123a322.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hello/index.html", {
+workbox.routing.registerNavigationRoute("/currency/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
